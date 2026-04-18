c271acfbe7c98406e0b167c41b2b721de7d38a42
tanejakeshav <tanejakeshav07@gmail.com>
2026-04-18 15:50:27 -0500

    News Collection Method

---
+++ briefly/backend/news_fetcher.py
@@ -0,0 +1,25 @@
+from newsapi import NewsApiClient
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
+from typing import List, Dict
+
+load_dotenv()
+
+newsapi = NewsApiClient(api_key=os.getenv("NewsAPI_KEY"))
+
+
+def get_news(query: str) -> List[Dict]:
+
+    response = newsapi.get_everything(
+        q=query,
+        language="en",
+        sort_by="publishedAt",
+        page_size=100
+    )
+
+    articles = response.get("articles", [])
+
+    print(articles)
+    return articles
+
